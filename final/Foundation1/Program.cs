d49fbb415f9fcd32bfcac35b9f22a0d70c76072a
mcqlein <ehigiemichael108@gmail.com>
2023-07-10 13:48:49 +0100

    Foundation Program #1 (Abstraction).

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,40 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        // Create videos and comments
+        Video video1 = new Video("Video 1", "Author 1", 120);
+        video1.AddComment("User 1", "Comment 1");
+        video1.AddComment("User 2", "Comment 2");
+
+        Video video2 = new Video("Video 2", "Author 2", 180);
+        video2.AddComment("User 3", "Comment 3");
+
+        // Create a list of videos
+        List<Video> videos = new List<Video>();
+        videos.Add(video1);
+        videos.Add(video2);
+
+        // Display video information and comments
+        foreach (Video video in videos)
+        {
+            Console.WriteLine("Title: " + video._title);
+            Console.WriteLine("Author: " + video._author);
+            Console.WriteLine("Length: " + video._length + " seconds");
+            Console.WriteLine("Number of Comments: " + video.GetNumberOfComments());
+
+            Console.WriteLine("Comments:");
+            foreach (Comment comment in video._comments)
+            {
+                Console.WriteLine("  " + comment._username + ": " + comment._text);
+            }
+
+            Console.WriteLine();
+        }
+
+        Console.ReadLine();
     }
 }
